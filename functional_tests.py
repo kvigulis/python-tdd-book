40c3c001f201d848e4811bc841fcae4e1c454cce
karlis <karlis@visioimpulse.com>
2019-09-11 20:51:01 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,21 @@
+from sys import platform
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
+
+if platform == "linux":
+	# Linux chrome driver
+	broser_driver_path = './chromedriver'
+else:
+	# Windows Chrome driver
+	broser_driver_path = './chromedriver.exe'
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
-		self.browser = webdriver.Chrome()
+		self.browser = webdriver.Chrome(broser_driver_path)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -17,9 +27,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1')
+		self.assertIn('To-Do', header_text)		
 
 		# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+		# Boatcow types "Nod my head" into a text box 
+		inputbox.send_keys('Nod my head')
+
+		# When she hits enter, the page updates, and now the page lists '1: Nod my head' as an item in a to-do list table
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.asserTrue(any(row.text == '1: Nod my head' for row in rows))
+
+		# There is still a text box inviting Boatcow to add another item. She enters 'Ask for food'
+		self.fail("Finish the test!")
+
 
 
 if __name__ == '__main__':

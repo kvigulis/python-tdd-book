2e11741ec4a514a6254621cdaf11a6481bc4b606
kvigulis <karlis.vigulis@city.ac.uk>
2019-09-12 00:15:18 +0100

    Fetching data from a POST request and returning it back to the same template

--- functional_tests.py
+++ functional_tests.py
@@ -43,12 +43,26 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Nod my head' for row in rows), 'New to-do item did not appear in table')
+		self.assertIn('1: Nod my head', [row.text for row in rows])
 
 		# There is still a text box inviting Boatcow to add another item. She enters 'Ask for food'
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Ask for food')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Nod my head', [row.text for row in rows])
+		self.assertIn('2: Ask for food', [row.text for row in rows])
+
+		# Boatcow wonder wehter the site will remember her list. The she sees
+		# that the site has generated a unique URL for her -- there is some 
+		# explanatory text to that effect
 		self.fail("Finish the test!")
 
-
+		# Boatcow visits that URL - her to-do list is still there.
 
 if __name__ == '__main__':
 	unittest.main()

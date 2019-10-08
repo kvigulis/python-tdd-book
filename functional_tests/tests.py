6f4a3c51670fca1fcd3703822be4f1c06f070f0f
kvigulis <karlis.vigulis@city.ac.uk>
2019-10-08 23:36:18 +0100

    Replaced 'voodoo' sleeps with more deterministic sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from sys import platform
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
@@ -13,6 +14,8 @@ else:
 	# Windows Chrome driver
 	broser_driver_path = './chromedriver.exe'
 
+MAX_WAIT = 10 	
+
 class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
@@ -20,6 +23,19 @@ class NewVisitorTest(LiveServerTestCase):
 
 	def tearDown(self):
 		self.browser.quit()
+
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Boatcow has heard about a cool new online to-do app. She goes 
@@ -40,23 +56,17 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# When she hits enter, the page updates, and now the page lists '1: Nod my head' as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Nod my head', [row.text for row in rows])
+		self.wait_for_row_in_list_table('1: Nod my head')
+		
 
 		# There is still a text box inviting Boatcow to add another item. She enters 'Ask for food'
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Ask for food')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Nod my head', [row.text for row in rows])
-		self.assertIn('2: Ask for food', [row.text for row in rows])
+		self.wait_for_row_in_list_table('1: Nod my head')
+		self.wait_for_row_in_list_table('2: Ask for food')
 
 		# Boatcow wonder wehter the site will remember her list. The she sees
 		# that the site has generated a unique URL for her -- there is some 
@@ -64,6 +74,3 @@ class NewVisitorTest(LiveServerTestCase):
 		self.fail("Finish the test!")
 
 		# Boatcow visits that URL - her to-do list is still there.
-
-if __name__ == '__main__':
-	unittest.main()
